399d3e891fa1f3bb64df5b33f83179653bd8d4cb
ikk2kk <ikk2kk@icloud.com>
2022-03-04 15:37:40 +0900

    ✨ コンポーネントを追加

---
+++ src/components/TodayTodo.tsx
@@ -0,0 +1,65 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { CREATE_TODO, GET_TODOS } from "queries/queries";
+import { TodoItem } from "src/components/TodoItem";
+import { TodoTitle } from "src/components/TodoTitle";
+import type { GetTodosQuery } from "types/generated/graphql";
+
+export const TodayTodo = () => {
+  const { data, error } = useQuery<GetTodosQuery>(GET_TODOS, {
+    fetchPolicy: "cache-first",
+  });
+  const [createTodo] = useMutation(CREATE_TODO, {
+    refetchQueries: [{ query: GET_TODOS }],
+  });
+
+  // const [todoItemList, setTodoItemList] = useState([
+  //   { id: "today0", text: "Next.jsのセットアップ" },
+  //   { id: "today1", text: "ESLintのインストール" },
+  // ]);
+  const addTodoItem = async (todoText: string) => {
+    // alert("AddTodoItem");
+    // setTodoItemList((prev) => {
+    //   const id = "today" + todoItemList.length;
+    //   return [...prev, { id: id, text: todoText }];
+    // });
+    try {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      await createTodo({ variables: { title: todoText, target_date: "abc", done: false } });
+      // alert("Add todo");
+      // reset();
+    } catch (error) {
+      console.error(error);
+      alert("Fail add todo");
+    }
+  };
+
+  if (error) {
+    return (
+      <div className="">
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 p-4 bg-red-100">
+      <TodoTitle title="今日する" className="mb-6 text-red-400 " />
+
+      <div className="space-y-3">
+        {data?.todos.map((todoItem) => {
+          return (
+            <TodoItem
+              key={todoItem.id}
+              addTodoItem={addTodoItem}
+              text={todoItem.title}
+              name={todoItem.id}
+              done={todoItem.done}
+              variant="red"
+            />
+          );
+        })}
+        <TodoItem addTodoItem={addTodoItem} text="" name="today_new" />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,159 @@
+import { useMutation } from "@apollo/client";
+import { DocumentDuplicateIcon, PlusSmIcon, TrashIcon } from "@heroicons/react/outline";
+import clsx from "clsx";
+import { DELETE_TODO, GET_TODOS, UPDATE_TODO } from "queries/queries";
+import type { ChangeEvent, FormEvent, VFC } from "react";
+import { useEffect, useState } from "react";
+import { Input } from "src/components/Input";
+
+type Props = {
+  addTodoItem: (todoText: string) => void;
+  text: string;
+  name: string;
+  done?: boolean;
+  variant?: "orange" | "yellow" | "red";
+};
+
+export const TodoItem: VFC<Props> = (props) => {
+  const [isChecked, setIsChecked] = useState<number>(0);
+  const [todoText, setTodoText] = useState("");
+
+  const [isFocus, setIsFocus] = useState(false);
+  const [updateTodo] = useMutation(UPDATE_TODO);
+  const [deleteTodo] = useMutation(DELETE_TODO, { refetchQueries: [{ query: GET_TODOS }] });
+
+  useEffect(() => {
+    setTodoText(props.text);
+    if (props.done === true) {
+      setIsChecked(1);
+    } else {
+      setIsChecked(0);
+    }
+  }, []);
+
+  const handleCheckBoxChange = async () => {
+    if (isChecked === 1) {
+      setIsChecked(0);
+      try {
+        await updateTodo({
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          variables: { id: props.name, title: todoText, target_date: "today", done: false },
+        });
+      } catch (error) {
+        alert("更新失敗");
+      }
+    } else {
+      setIsChecked(1);
+      try {
+        await updateTodo({
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          variables: { id: props.name, title: todoText, target_date: "today", done: true },
+        });
+      } catch (error) {
+        alert("更新失敗");
+      }
+    }
+  };
+
+  const handleInputFocus = () => {
+    setIsFocus(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsFocus(false);
+    if (todoText.length === 0) {
+      setTodoText(props.text);
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    return setTodoText(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (todoText.length !== 0 && props.name.match("new")) {
+      props.addTodoItem(todoText);
+      setTodoText("");
+      // console.log("new");
+    } else if (todoText.length !== 0) {
+      // console.log("update");
+      try {
+        await updateTodo({
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          variables: { id: props.name, title: todoText, target_date: "today", done: isChecked ? true : false },
+        });
+      } catch (error) {
+        alert("更新失敗");
+      }
+    }
+  };
+
+  const handleDuplicate = () => {
+    alert("Duplicate " + props.name);
+  };
+  const handleTrash = async () => {
+    // alert("Trash " + props.name);
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      await deleteTodo({ variables: { id: props.name } });
+      // alert("Add todo");
+      // reset();
+    } catch (error) {
+      // console.error(error);
+      alert("Fail delete todo");
+    }
+  };
+
+  return (
+    <form className="flex items-center " onSubmit={handleSubmit}>
+      <label className="mr-4 text-gray-800 hover:cursor-pointer" htmlFor={props.name}>
+        <div className="flex items-center bg-green-100">
+          <input
+            type="checkbox"
+            className={clsx(
+              "inline-block w-4 h-4 rounded-full border-0 ring-2 ring-gray-300 ring-offset-2 appearance-none hover:cursor-pointer",
+              { "bg-orange-500": props.variant === "orange" && isChecked === 1 },
+              { "bg-yellow-400": props.variant === "yellow" && isChecked === 1 },
+              { "bg-red-500": props.variant === "red" && isChecked === 1 },
+
+              { "bg-white": isChecked === 0 },
+              {
+                "bg-gray-300 ring-4 ring-offset-0 ": todoText.length === 0 && isFocus === false,
+              }
+            )}
+            disabled={isFocus && todoText.length === 0}
+            value={isChecked}
+            // onClick={handleClick}
+            onChange={handleCheckBoxChange}
+          />
+          {todoText.length === 0 && isFocus === false ? (
+            <PlusSmIcon className="absolute w-4 h-4 text-white bg-gray-300" />
+          ) : null}
+        </div>
+      </label>
+
+      <div className="group container relative">
+        <Input
+          label=""
+          linethrough={isChecked}
+          variant={props.variant}
+          name={props.name}
+          type="text"
+          placeholder="タスクを追加する"
+          value={todoText}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+        />
+        {props.name.match("new") ? null : (
+          <div className="flex absolute inset-y-0 right-1 items-center my-auto space-x-5 h-full opacity-0 group-hover:opacity-100">
+            <DocumentDuplicateIcon className="w-5 h-5 text-gray-400 cursor-pointer" onClick={handleDuplicate} />
+            <TrashIcon className="w-5 h-5 text-gray-400 cursor-pointer" onClick={handleTrash} />
+          </div>
+        )}
+      </div>
+    </form>
+  );
+};
