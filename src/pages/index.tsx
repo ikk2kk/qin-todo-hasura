8b935a8ccb6fdd0a7fc3041c38d1cfa2670ccc7e
ikk2kk <ikk2kk@icloud.com>
2022-03-17 07:14:40 +0900

    ♻️ リファクタ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import type { DragEndEvent, DragOverEvent, DragStartEvent, Over } from "@dnd-kit/core";
 import { DndContext } from "@dnd-kit/core";
 import { closestCorners } from "@dnd-kit/core";
@@ -6,20 +6,29 @@ import { KeyboardSensor, MouseSensor, PointerSensor, useSensor, useSensors } fro
 import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { CREATE_TODOS, GET_TODOS } from "queries/queries";
+import { CREATE_TODOS, GET_TODOS_ALL } from "queries/queries";
 import { useEffect, useState } from "react";
+import { IdTodoDicVar } from "src/cache";
 import { Layout } from "src/components/layout";
 import { SomedayTodo } from "src/components/SomedayTodo";
 import { TodayContainer } from "src/components/TodayContainer";
 // import { TodayTodo } from "src/components/TodayTodo";
 import { TomorrowTodo } from "src/components/TomorrowTodo";
-import type { Todos } from "types/generated/graphql";
+import type { GetTodosQuery, Todos } from "types/generated/graphql";
 
-type Items = {
-  today: string[];
-  tomorrow: string[];
-  someday: string[];
+// type Items = {
+//   today: string[];
+//   tomorrow: string[];
+//   someday: string[];
+// };
+
+type IdListByCategory = {
+  [key in Categories]: string[];
 };
+
+// type TodoListByCategory = {
+//   [key in Categories]: Todos[];
+// };
 
 type TodoListObject = {
   today: Todos[];
@@ -30,11 +39,56 @@ type TodoListObject = {
 type Categories = "today" | "tomorrow" | "someday";
 
 const Home: NextPage = () => {
-  const [items, setItems] = useState<Items>({
-    today: ["1", "2", "3"],
-    tomorrow: ["4", "5", "6"],
-    someday: ["7", "8", "9"],
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { data, error, loading } = useQuery<GetTodosQuery>(GET_TODOS_ALL, {
+    fetchPolicy: "cache-first",
   });
+
+  const [idListByCategory, setIdListByCategory] = useState<IdListByCategory>({
+    today: [],
+    tomorrow: [],
+    someday: [],
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [idTodoDic, setIdTodoDic] = useState<{ [key: string]: Todos }>({});
+
+  useEffect(() => {
+    if (!loading) {
+      // idListByCategory
+      Object.keys(idListByCategory).forEach((category) => {
+        const temp = data?.todos.filter((todo) => {
+          return todo["target_date"] === category;
+        });
+
+        if (temp) {
+          const temp2: string[] = temp?.map((todo) => {
+            return todo["id"];
+          });
+
+          setIdListByCategory((prev) => {
+            return { ...prev, [category]: temp2 };
+          });
+        }
+      });
+
+      // idTodoDic
+      if (data) {
+        const tempIdTodoDic: { [key: string]: Todos } = {};
+        data.todos.forEach((todo) => {
+          return (tempIdTodoDic[todo.id] = todo);
+        });
+        IdTodoDicVar(tempIdTodoDic);
+        setIdTodoDic(tempIdTodoDic);
+      }
+    }
+  }, [data, loading]);
+
+  // const [items, setItems] = useState<Items>({
+  //   today: ["1", "2", "3"],
+  //   tomorrow: ["4", "5", "6"],
+  //   someday: ["7", "8", "9"],
+  // });
 
   const [todoListObj, setTodoListObj] = useState<TodoListObject>({
     today: [],
@@ -43,7 +97,7 @@ const Home: NextPage = () => {
   });
   const [createTodos] = useMutation(CREATE_TODOS, {
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    refetchQueries: [{ query: GET_TODOS, variables: { target_date: "today" } }],
+    refetchQueries: [{ query: GET_TODOS_ALL }],
   });
   // const [createTodos] = useMutation(CREATE_TODOS);
   const [isIndexChanged, setIsIndexChanged] = useState(false);
@@ -83,54 +137,39 @@ const Home: NextPage = () => {
   };
   useEffect(() => {
     if (isIndexChanged) {
-      // todoListObj => {id: Todo}
       const dic: { [key: string]: Todos } = {};
       todoListObj["today"].forEach((e) => {
         return (dic[e.id] = e);
       });
-      // console.log(dic);
-      //
-      const sorted_items: Pick<Todos, "id" | "title" | "target_date" | "done" | "order_index">[] = items["today"].map(
-        (e, index) => {
+      const sorted_items: Pick<Todos, "id" | "title" | "target_date" | "done" | "order_index">[] = idListByCategory[
+        "today"
+      ].map((e, index) => {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        return {
+          id: dic[e].id,
+          title: dic[e].title,
           // eslint-disable-next-line @typescript-eslint/naming-convention
-          // return { ...dic[e], order_index: index };
-          return {
-            id: dic[e].id,
-            title: dic[e].title,
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            target_date: dic[e].target_date,
-            done: dic[e].done,
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            order_index: index,
-          };
-        }
-      );
-      // console.log(sorted_items);
+          target_date: dic[e].target_date,
+          done: dic[e].done,
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          order_index: index,
+        };
+      });
       // Batch Write
       handleTodos(sorted_items);
-      // try {
-      //   await createTodos({
-      //     variables: {
-      //       objects: sorted_items
-      //     }
-      //   })
-      // } catch(error) {
-      //   console.error(error)
-      //   alert("Fail Upsert")
-      // }
 
       setIsIndexChanged(false);
     }
-  }, [items, isIndexChanged]);
+  }, [idListByCategory, isIndexChanged]);
 
   const findContainer = (id: string) => {
-    if (id in items) {
+    if (id in idListByCategory) {
       return id;
     }
 
-    return Object.keys(items).find((key) => {
-      const tkey: keyof Items = key as Categories;
-      return items[tkey].includes(id);
+    return Object.keys(idListByCategory).find((key) => {
+      const tkey: keyof IdListByCategory = key as Categories;
+      return idListByCategory[tkey].includes(id);
     });
   };
   const handleDragStart = (event: DragStartEvent) => {
@@ -154,7 +193,7 @@ const Home: NextPage = () => {
       return;
     }
 
-    setItems((prev) => {
+    setIdListByCategory((prev) => {
       const activeItems = prev[activeContainer as Categories];
       const overItems = prev[overContainer as Categories];
 
@@ -183,7 +222,7 @@ const Home: NextPage = () => {
         ],
         [overContainer]: [
           ...prev[overContainer as Categories].slice(0, newIndex),
-          items[activeContainer as Categories][activeIndex],
+          idListByCategory[activeContainer as Categories][activeIndex],
           ...prev[overContainer as Categories].slice(newIndex, prev[overContainer as Categories].length),
         ],
       };
@@ -201,11 +240,11 @@ const Home: NextPage = () => {
       return;
     }
 
-    const activeIndex = items[activeContainer as Categories].indexOf(active.id);
-    const overIndex = items[overContainer as Categories].indexOf(overId);
+    const activeIndex = idListByCategory[activeContainer as Categories].indexOf(active.id);
+    const overIndex = idListByCategory[overContainer as Categories].indexOf(overId);
 
     if (activeIndex !== overIndex) {
-      setItems((items) => {
+      setIdListByCategory((items) => {
         return {
           ...items,
           [overContainer]: arrayMove(items[overContainer as Categories], activeIndex, overIndex),
@@ -215,7 +254,13 @@ const Home: NextPage = () => {
     }
     setActiveId(null);
   };
-
+  if (error) {
+    return (
+      <div className="">
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
   // console.log(items);
   return (
     <Layout>
@@ -231,7 +276,12 @@ const Home: NextPage = () => {
           onDragOver={handleDragOver}
           onDragEnd={handleDragEnd}
         >
-          <TodayContainer id="today" items={items.today} setItems={setItems} setTodoListObj={setTodoListObj} />
+          <TodayContainer
+            id="today"
+            items={idListByCategory.today}
+            setItems={setIdListByCategory}
+            setTodoListObj={setTodoListObj}
+          />
           {/* <TodayTodo /> */}
           <TomorrowTodo />
           <SomedayTodo />
