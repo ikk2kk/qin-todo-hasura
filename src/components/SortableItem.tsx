00897dd231d19249073a2be4e6792402f939cdc7
ikk2kk <ikk2kk@icloud.com>
2022-03-18 15:17:14 +0900

    🐛 scaleが勝手に変更されるため、1.0固定値を代入

--- src/components/SortableItem.tsx
+++ src/components/SortableItem.tsx
@@ -10,6 +10,11 @@ type Props = {
 
 export const SortableItem: VFC<Props> = (props) => {
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: props.id });
+  if (transform) {
+    transform.scaleX = 1.0;
+    transform.scaleY = 1.0;
+  }
+
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
